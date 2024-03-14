4155046069ffc755d2e029e08fa21cda4318256e
Alessandro Pioggia <73896611+AlePioggia@users.noreply.github.com>
2024-03-14 11:54:54 +0100

    TDD: complete last step of Task4

--- src/main/scala/u02/exercises/Task4.scala
+++ src/main/scala/u02/exercises/Task4.scala
@@ -2,13 +2,21 @@ package u02.exercises
 
 object Task4 extends App:
   enum Shape:
-    case Rectangle(base: Double, height: Double)
     case Square(base: Double)
+    case Rectangle(base: Double, height: Double)
     case Circle(radius: Double)
 
   object ShapeUtils {
     def perimeter(shape: Shape): Double = shape match
-      case Shape.Rectangle(base, height) => base * 2 + height * 2
       case Shape.Square(base)            => base * 4
+      case Shape.Rectangle(base, height) => base * 2 + height * 2
       case Shape.Circle(radius)          => radius * Math.PI * 2
+
+    def contains(shape: Shape, point: (Double, Double)) = (shape, point) match
+      case (Shape.Square(base), (x, y)) => Math.sqrt(x * x + y * y) < base
+      case (Shape.Rectangle(base, height), (x, y)) =>
+        x >= 0 && x <= base && y >= 0 && y <= height
+      case (Shape.Circle(radius), (x, y)) =>
+        x >= 0 && x <= radius && y >= 0 && y <= radius
+
   }
--- src/test/scala/u02/Task4Test.scala
+++ src/test/scala/u02/Task4Test.scala
@@ -8,8 +8,20 @@ import u02.exercises.Task4.ShapeUtils.*
 class Task4Test {
 
   @Test def testPerimeter(): Unit = {
-    assertEquals(perimeter(Rectangle(2, 4)), 12, 0)
     assertEquals(perimeter(Square(4)), 16, 0)
+    assertEquals(perimeter(Rectangle(2, 4)), 12, 0)
     assertEquals(perimeter(Circle(4)), 25, 1)
+  }
+
+  @Test def testContainsWithBoundedPoints(): Unit = {
+    assertTrue(contains(Square(4), (2, 2)))
+    assertTrue(contains(Rectangle(4, 5), (3, 3)))
+    assertTrue(contains(Circle(4), (2, 2)))
+  }
+
+  @Test def testContainsWithUnboundedPoints(): Unit = {
+    assertFalse(contains(Square(4), (4, 5)))
+    assertFalse(contains(Rectangle(4, 5), (5, 6)))
+    assertFalse(contains(Circle(4), (6, 6)))
   }
 }
